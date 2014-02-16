ca53c5c913d64f988544ac9445c4eeedab26d9e6
Rick Walker <walker@omnisterra.com>
2014-02-15 21:27:37 -0800

    progress checkin

--- pick.c
+++ pick.c
@@ -1,18 +1,17 @@
 #include <stdio.h>
 
 extern int bflag;   /* => 1, if we are in the background, 0 otherwise */
+extern int fflag;   /* => 0, if we are set quiet */
 
 pick(s) /* offer choice of s */
 char *s;
 {
-    if (s[0])           /* format tweaking! (just like /bin/rm) */
-        fprintf(stderr, "%s: ? ",s);
-    else 
-        fprintf(stderr, " ? ");
+    fprintf(stderr, "%s: ? ",s);
 
-    if (bflag) {        /* if in the background, return 0 */
-        fprintf(stderr, "n\n");
+    if (bflag)          /* if in the background, return 0 */
         return(0);      /* don't call ttyin if in background!! */
-    } else 
-        return (ttyin() == 'y');
+
+    if (fflag  && (ttyin() == 'y'))
+        return(1);
+    return(0);          /* if set quiet, return 0 */
 }
